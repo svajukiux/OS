d2e013aedc92fb50b1e111b28ebf34c5e2b09440
svka4019 <svajunas.kavaliauskas@mif.stud.vu.lt>
2019-04-04 11:25:16 +0300

    Arithmetic commands and compare added

--- OS/src/VM.java
+++ OS/src/VM.java
@@ -95,23 +95,136 @@ public class VM {
 			return false;
 		}
 		switch (String.valueOf(startingPosition.getBytes())) {
-			case "ADD ":  // arba pakeisti kad visos butu 4 daliklio ilgumo
+			case "ADD ": {  // arba pakeisti kad visos butu 4 daliklio ilgumo 
 				PC++;
 				int stack = Integer.parseInt(String.valueOf(SP));
-				int stackAddress = Integer.parseInt(String.valueOf(SP));
+				//int stackAddress = Integer.parseInt(String.valueOf(SP));
 				Word pirmas = memory[stack/16][stack%16];
 				Word antras = memory[(stack-1)/16][(stack-1)%16];
+				
 				int suma = pirmas.toInt() + antras.toInt();
-				// TODO flagu nustatymai pagal rezultata;
+				boolean CF=false,OF=false,ZF = false;
+				if(suma==0) {
+					ZF=true;
+				}
+				if(suma < pirmas.toInt() || suma < antras.toInt()) {
+					CF= true;
+					OF= true; 
+				}
+				setSF(CF,ZF,OF);
+				
 				Word toPut = new Word(Integer.toHexString(suma).toCharArray());
 				memory[(stack-1)/16][(stack-1)%16]=toPut;
 				SP--;	
 				return true;
+			}
+				
+			case "SUB ": {
+				PC++;
+				int stack = Integer.parseInt(String.valueOf(SP));
+				//int stackAddress = Integer.parseInt(String.valueOf(SP));
+				Word pirmas = memory[stack/16][stack%16];
+				Word antras = memory[(stack-1)/16][(stack-1)%16];
+				int skirtumas = antras.toInt() - pirmas.toInt();
+				boolean CF=false,OF=false,ZF = false;
+				if(skirtumas==0) {
+					ZF=true;
+				}
+				if(antras.toInt() < pirmas.toInt()) {
+					CF= true;
+					 
+				}
+				if(skirtumas > antras.toInt()) {
+					OF=true;
+				}
+				setSF(CF,ZF,OF);
+				
+				Word toPut = new Word(Integer.toHexString(skirtumas).toCharArray());
+				memory[(stack-1)/16][(stack-1)%16]=toPut;
+				SP--;	
+				return true;
+			}
+			case "MUL ": {
+				PC++;
+				int stack = Integer.parseInt(String.valueOf(SP));
+				//int stackAddress = Integer.parseInt(String.valueOf(SP));
+				Word pirmas = memory[stack/16][stack%16];
+				Word antras = memory[(stack-1)/16][(stack-1)%16];
+				int sandauga = pirmas.toInt() * antras.toInt();
+				boolean CF=false,OF=false,ZF = false;
+				if(sandauga==0) {
+					ZF=true;
+				}
+				if(sandauga < pirmas.toInt() || sandauga < antras.toInt()) {
+					CF= true;
+					OF=true;
+					 
+				}
+				
+				setSF(CF,ZF,OF);
+				
+				Word toPut = new Word(Integer.toHexString(sandauga).toCharArray());
+				memory[(stack-1)/16][(stack-1)%16]=toPut;
+				SP--;	
+				return true;
+				
+			}
+			case "DIV ": {
+				PC++;
+				int stack = Integer.parseInt(String.valueOf(SP));
+				//int stackAddress = Integer.parseInt(String.valueOf(SP));
+				Word pirmas = memory[stack/16][stack%16];
+				Word antras = memory[(stack-1)/16][(stack-1)%16];
+				int dalyba = antras.toInt() / pirmas.toInt();
+				boolean CF=false,OF=false,ZF = false;
+				if(dalyba==0) {
+					ZF=true;
+				}
+				if(antras.toInt() < pirmas.toInt()) {
+					CF= true;
+					OF=true;
+					 
+				}
+				
+				setSF(CF,ZF,OF);
+				
+				Word toPut = new Word(Integer.toHexString(dalyba).toCharArray());
+				memory[(stack-1)/16][(stack-1)%16]=toPut;
+				SP--;	
+				return true;
+			}
+			case "CMP ": {
+				PC++;
+				int stack = Integer.parseInt(String.valueOf(SP));
+				//int stackAddress = Integer.parseInt(String.valueOf(SP));
+				Word pirmas = memory[stack/16][stack%16];
+				Word antras = memory[(stack-1)/16][(stack-1)%16];
+				boolean CF=false,OF=false,ZF = false;
+				if(antras.toInt()==pirmas.toInt()) {
+					ZF=true;
+				}
+				if(antras.toInt()<pirmas.toInt()) {
+					CF=true;
+				}
+				/// TODO kiekvienoj komandoj timeri decrementint
+				setSF(CF,ZF,OF);
+				
+				return true;
+			}
+			
+				
 			
 		}
 		
 		
 		return true;
+	}
+	
+	
+	private void setSF(boolean CF, boolean ZF, boolean OF) {
+		SF = (char) (CF==true ? (SF | (1 << 0)) : (SF & ~(1 << 0)));
+		SF = (char) (ZF==true ? (SF | (1 << 1)) : (SF & ~(1 << 1)));
+		SF = (char) (OF==true ? (SF | (1 << 2)) : (SF & ~(1 << 2)));
 	}
 	
 
